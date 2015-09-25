ae1e5abe74e75a2f31de9ee41daebd6c7db16f49
htckandi <test@tests-mac.local>
2015-09-24 23:22:24 -0700

    Initial Commit

---
+++ test_yandex_weatherTests/test_yandex_weatherTests.swift
@@ -0,0 +1,36 @@
+//
+//  test_yandex_weatherTests.swift
+//  test_yandex_weatherTests
+//
+//  Created by Test on 9/24/15.
+//  Copyright © 2015 Test. All rights reserved.
+//
+
+import XCTest
+@testable import test_yandex_weather
+
+class test_yandex_weatherTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
